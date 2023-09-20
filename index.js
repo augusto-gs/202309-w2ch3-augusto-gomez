0cc2825c648afca7364f2ec0e68902862f011dbd
augusto-gs <143880904+augusto-gs@users.noreply.github.com>
2023-09-20 19:19:05 +0200

    Add function ro remove song from playlist by title

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
 const songs = [
   {
+    id: 1,
     title: "Rush",
     artist: "Troye Sivan",
     genre: "Pop",
+    duration: 400,
     year: 2023,
     hit: true,
   },
   {
+    id: 2,
     title: "Flowers",
     artist: "Miley Cyrus",
     genre: "Pop",
+    duration: 200,
     year: 2023,
     hit: true,
   },
   {
+    id: 3,
     title: "On My Love",
     artist: "Zara Larsson",
     genre: "Electronic",
+    duration: 300,
     year: 2022,
     hit: false,
   },
   {
+    id: 4,
     title: "Jolene",
     artist: "Dolly Parton",
     genre: "Country",
+    duration: 180,
     year: 1980,
     hit: false,
   },
   {
+    id: 5,
     title: "Creep",
     artist: "Radiohead",
     genre: "Rock",
+    duration: 180,
     year: 1993,
     hit: true,
   },
 ];
 
 const addToPlaylist = (song) => songs.push(song);
+
+const removeFromPlaylist = (title) => {
+  const removedSong = songs.find(
+    (song) => song.title.toLowerCase() === title.toLowerCase()
+  );
+
+  const positionOfRemovedSong = songs.indexOf(removedSong);
+
+  songs.splice(positionOfRemovedSong, 1);
+};
